99dce71fc27fd0cb21cf91717dc882ae92f53468
suvendu laha <laha.suvendu@gmail.com>
2019-08-22 21:29:06 -0700

    Second message

--- LuaSampleApp/Program.cs
+++ LuaSampleApp/Program.cs
@@ -9,7 +9,7 @@ namespace LuaSampleApp
         {
             Console.WriteLine("Hello World!");
             Lua state = new Lua();
-            var res = state.DoString("return 10 + 3*(5 + 2)")[0];
+            var res = state.DoString("return 10 + 3*(5 + 2)")[0]; 
             Console.WriteLine("Result :" + res);
             state.LoadCLRPackage();
             state.DoString(@" import ('LuaSampleApp', 'LuaSampleApp') 
@@ -17,7 +17,7 @@ namespace LuaSampleApp
             Console.WriteLine("Result before Script :" + SampleObject.GetValue("c"));
             state.DoString(@"
 	        local res4 = SampleObject.GetValue('b')
-            res4 = res4 * res4
+            res4 = res4 * res4 * res4
             SampleObject.SetValue('c', res4);
 	        ");
             Console.WriteLine("Result After Script :" + SampleObject.GetValue("c"));
